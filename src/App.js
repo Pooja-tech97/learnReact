206bd18ca836930b2c723bc124fc6786c63bcca8
Pooja Rani <pr611732@gmail.com>
2025-03-06 00:32:58 +0530

    load random cat images

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import React from "react"
+import './App.css';
+import LoadImages from "./compnents/homePage"
+
+function App() {
+  return (
+  <>
+      <h1>Cute cat Images</h1>
+      <LoadImages/>
+  </>
+  );
+}
+
+export default App;
